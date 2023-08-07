7bda1ee4662db7ea12aec4f10c4cb2c90e1dd1f5
Abdou <2000abdallah2014@gmail.com>
2023-08-07 09:20:33 +0100

    dynamically generate the width of the items

---
+++ components/buttons.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="btn-container">
+        <div class="seperated-cont" v-if="showSeperated">
+            <div class="plus-btns-cont"><v-btn v-for="btn in gameStore.gameSettings.buttons">{{ btn }}</v-btn></div>
+            <div class="minus-btns-cont"><v-btn v-for="btn in gameStore.gameSettings.buttons">{{ btn }}</v-btn></div>
+        </div>
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useGameStore } from '@/store/GameStore'
+const gameStore = useGameStore()
+const props = defineProps({
+    width: { type: Number, required: true },
+    index: { type: Number, required: true },
+})
+
+const showSeperated = computed((): boolean => {
+    const btnsNumber = gameStore.gameSettings.buttons.length;
+    return 65 * btnsNumber + 40 < props.width ? true : false
+})
+</script>
+
--- pages/playground.vue
+++ pages/playground.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="container flex gap-4">
-        <div v-for="(team, index) in gameStore.teams">
-            <v-avatar size="x-large" class="avatar-cont relative">
-                <div v-html="team.avatar" class="w-full h-full absolute "></div>
-
-            </v-avatar>
-            <p> {{ team.name }}</p>
+    <div class="playground-container flex gap-4 flex-wrap" ref="container">
+        <div v-for="(team, index) in gameStore.teams"
+            :style="{ 'background-color': team.color, 'width': widths[index] + 'px' }">
+            <div class="flex">
+                <v-avatar size="x-large" class="avatar-cont relative">
+                    <div v-html="team.avatar" class="w-full h-full absolute "></div>
+                </v-avatar>
+                <p> {{ team.name }}</p>
+            </div>
+            <p>{{ team.points }}</p>
+            <div class="btn-container flex gap-1">
+                <v-btn v-for="btn in gameStore.gameSettings.buttons">{{ btn }}</v-btn>
+            </div>
 
         </div>
     </div>
@@ -14,8 +20,53 @@
 import { useGameStore } from '@/store/GameStore'
 const gameStore = useGameStore()
 
-const avatarSrcGenerator = (index: number) => {
-    return 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(gameStore.teams[index].avatar)
-}
+const container = ref<HTMLInputElement | null>(null);
+let playgroundWidth: Ref<number> = ref(300);
 
+
+//breakpoints
+const breakpoints = useBreakpoints({
+    phone: 480,
+    tablet: 768,
+    // laptop: 1024,
+})
+const smallVw = breakpoints.smallerOrEqual('phone');
+const midVw = breakpoints.between("phone", "tablet");
+const largeVw = breakpoints.greaterOrEqual("tablet");
+
+const widths: Ref<number[]> = ref([])
+
+const sigleWidthGenerator = (index: number) => {
+    let schema: number[] = largeVw.value ? [1, 2, 3] : midVw.value ? [2, 3] : [3]
+
+
+    let res: number = 1;
+
+    for (let i = 0; i < schema.length; i++) {
+        let currentValue = schema[0]
+        //find the sum to the current i
+        schema.reduce((acc, cr, cIndex) => {
+            if (cIndex === i) { currentValue = (acc + cr) }
+            return acc + cr
+        })
+
+        if (index < currentValue) {
+            res = schema[i]
+            break;
+        } else { res = schema[schema.length - 1] }
+    }
+    const width = ((playgroundWidth.value - (16 * (res - 1))) / res)
+    widths.value.push(width)
+    return res
+
+
+}
+onMounted(() => {
+
+    playgroundWidth.value = container.value?.getBoundingClientRect().width ?? 300;
+
+    gameStore.teams.forEach((el, index) => sigleWidthGenerator(index))
+
+    console.log(playgroundWidth.value, widths.value)
+})
 </script>
